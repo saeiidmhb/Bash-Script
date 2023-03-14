e7406b1ff65dc59928f060d701c14568208c1556
Saeed Mohebbi <saeiidmhb@gmail.com>
2023-03-14 03:18:18 -0500

    first commit

--- Check_Os.sh
+++ Check_Os.sh
@@ -6,11 +6,9 @@ CHECK=$(cat /etc/os-release | grep -i id_like | tr '[:upper:]' '[:lower:]')
 if [[ $CHECK == *$OS1* ]]
 then
     echo "It's Debian."
-    apt update
 elif [[ $CHECK == *$OS2* ]]
 then
     echo "It's Centos."
-    yum update
 else
-    echo "Didn't recognise the os."
+    echo "Didn't recognize the os."
 fi
